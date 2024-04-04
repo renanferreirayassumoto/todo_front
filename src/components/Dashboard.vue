5baf1e585c3aa220608231b0472dbba868986241
renanferreirayassumoto <renankosmo@gmail.com>
2024-04-04 10:19:27 -0400

    feat: estilização drawer lateral

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,25 +1,87 @@
 <template>
-	<v-navigation-drawer permanent>
+	<v-navigation-drawer width="300" permanent>
 		<v-list-item
-			prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-			:title="current_user"
-			nav
+			class="pt-10 d-flex justify-center"
+			prepend-avatar="https://picsum.photos/200"
 		>
+			<template v-slot:prepend>
+				<v-avatar size="70"></v-avatar>
+			</template>
+			<v-list-item-title class="text-subtitle-1 font-weight-thin"
+				>To-Do</v-list-item-title
+			>
+			<v-list-item-subtitle class="text-h4 text-red-accent-2">{{
+				current_user
+			}}</v-list-item-subtitle>
 		</v-list-item>
 
-		<v-divider></v-divider>
+		<v-divider
+			thickness="4"
+			color="red-accent-2"
+			class="border-opacity-100 w-66 mx-auto mb-16 mt-12"
+		></v-divider>
 
-		<v-list density="compact" nav>
-			<v-list-item
-				prepend-icon="mdi-calendar-blank"
-				title="Tarefas para hoje"
-				value="dailyTasks"
-			></v-list-item>
-			<v-list-item
-				prepend-icon="mdi-calendar-clock-outline"
-				title="Tarefas agendadas"
-				value="scheduledTasks"
-			></v-list-item>
+		<v-list>
+			<v-list-group>
+				<template v-slot:activator="{ props }">
+					<v-list-item v-bind="props" class="mx-2">
+						<template v-slot:prepend>
+							<v-icon
+								icon="mdi-calendar-outline"
+								color="red-accent-2"
+								size="x-large"
+							></v-icon>
+						</template>
+						<v-list-item-title class="text-subtitle-1"
+							>Tarefas para hoje</v-list-item-title
+						></v-list-item
+					>
+				</template>
+
+				<v-list-item v-bind="props"
+					><template v-slot:prepend>
+						<v-icon
+							icon="mdi-circle"
+							size="x-small"
+							class="mr-4 ml-4 mr-n3"
+							color="purple"
+						></v-icon> </template
+					><v-list-item-title>Financeiro</v-list-item-title></v-list-item
+				>
+				<v-list-item v-bind="props"
+					><template v-slot:prepend>
+						<v-icon
+							icon="mdi-circle"
+							size="x-small"
+							class="mr-4 ml-4 mr-n3"
+							color="yellow"
+						></v-icon> </template
+					><v-list-item-title>Pessoal</v-list-item-title></v-list-item
+				>
+				<v-list-item v-bind="props"
+					><template v-slot:prepend>
+						<v-icon
+							icon="mdi-circle"
+							size="x-small"
+							class="mr-4 ml-4 mr-n3"
+							color="blue"
+						></v-icon> </template
+					><v-list-item-title>Saúde</v-list-item-title></v-list-item
+				>
+			</v-list-group>
+
+			<v-list-item to="/scheduled" class="mx-2 mt-5">
+				<template v-slot:prepend>
+					<v-icon
+						icon="mdi-calendar-clock"
+						color="red-accent-2"
+						size="x-large"
+					></v-icon>
+				</template>
+				<v-list-item-title class="text-subtitle-1"
+					>Tarefas agendadas</v-list-item-title
+				>
+			</v-list-item>
 		</v-list>
 	</v-navigation-drawer>
 
